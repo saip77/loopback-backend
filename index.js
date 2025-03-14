73c86220044758122ba43fe3ddbce2d98bc54269
SAI PRASAD <sp12961369@gmail.com>
2025-03-15 00:24:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5500;
+app.get("/:id", (req,res)=>{
+    const id = req.params.id ;
+    res.send(`Hi your user id is ${id}`)
+})
+app.listen(port, ()=>{
+    console.log(`Running on port ${port}`)
+})
