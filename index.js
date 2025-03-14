4c2f4f332975bcff9922bc601a42d809c2c50f36
Sai Prasad <sai.a.prasad@pwc.com>
2025-03-15 01:16:35 +0530

    ID changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv")
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5500;
-app.get("/:id", (req,res)=>{
+app.get('/:id', (req,res)=>{
     const id = req.params.id ;
     res.send(`Hi your user id is ${id}`)
 })
